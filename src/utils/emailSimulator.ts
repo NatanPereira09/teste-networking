311203c4bd311afb5a795a559c37eb9659158155
NatanPereira09 <91692824+NatanPereira09@users.noreply.github.com>
2025-11-11 20:02:15 -0300

    Revert "Initial commit"
    This reverts commit 38b38f56ab21c124f3c874ec9504d7acd54547a9.

--- src/utils/emailSimulator.ts
+++
@@ -1,4 +0,0 @@
-export function simulateSendEmail(to, subject, body) {
-  console.info("SIMULATED EMAIL ->", { to, subject, body });
-  return { ok: true, previewLink: (body || '').match(/https?:\/\/[^\s]+/)?.[0] ?? null };
-}
